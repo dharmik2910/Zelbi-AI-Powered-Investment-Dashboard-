66830f50d1b426054e85b078937a0504e0473b7e
Dharmik Rabadiya <djrabadiya2910@gmail.com>
2026-06-14 11:22:29 +0530

    Update mailSender.js

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -5,15 +5,15 @@ dotenv.config();
 
 const mailSender = async (email, title, body) => {
   try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.MAIL_HOST,
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-      },
-    });
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
 
     // Check SMTP connection
     await transporter.verify();
